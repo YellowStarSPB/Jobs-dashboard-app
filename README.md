1f389bdbc325f487cba627b64dbe2c3492b55ef6
YellowSar <hsgopoke@gmail.com>
2023-05-03 15:48:48 +0300

    app completed

---
+++ README.md
@@ -0,0 +1,5 @@
+This dashboard for search job in IT sphere
+
+I used in this project mock data, not send request in server and database
+
+Mock path - './first-project/src/mock/data.json'
---
+++ src/components/FilterPanel.jsx
@@ -0,0 +1,37 @@
+import { useSelector, useDispatch } from 'react-redux';
+import { Badge } from 'UI/Badge';
+import { Card } from 'UI/Card';
+import { Stack } from 'UI/Stack';
+
+import { selectFilters } from 'store/filters/filter-selectors';
+import { clearFilter, removeFilter } from 'store/filters/filter-actions';
+
+const FilterPanel = () => {
+    const dispatch = useDispatch()
+    const currentFilters = useSelector(selectFilters)
+
+
+    if (currentFilters.length === 0) return null
+
+    return (
+        <Card className="filter-panel">
+            <div className="filter-panel-wrapper">
+                <Stack>
+                    {currentFilters.map(filter => (
+                        <Badge
+                            onClear={() => dispatch(removeFilter(filter))}
+                            key={filter}
+                            variant="clearable"
+                        >
+                            {filter}
+                        </Badge>
+                    ))}
+                </Stack>
+
+                <button onClick={() => dispatch(clearFilter)} className='link'>Clear</button>
+            </div>
+        </Card>
+    )
+}
+
+export { FilterPanel };
